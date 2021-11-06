1d6423fbac9f0a15a4da1dd5985ca8c62d9b4f5b
nt <nikola.tulechki@gmail.com>
2021-11-06 10:50:19 +0200

    cands init

---
+++ data/flatten.py
@@ -0,0 +1,25 @@
+import csv
+
+out = []
+with open("Appearances-Grid view.csv", "r") as f:
+    data = csv.reader(f)
+    next(data)
+    for row in data:
+        pref = [row[5],row[7],row[6]]
+        cands = row[11].split(",")
+        partys = row[14].split(",")
+
+        for i, v in enumerate(cands):
+                tmprow = []
+                tmprow.extend(pref)
+                tmprow.append(v)
+                tmprow.append(partys[i])
+                tmprow.append(row[3])
+
+                out.append(tmprow)
+
+with open('tv_cands.csv', mode='w') as out_file:
+    w = csv.writer(out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    w.writerow(["Дата", "Телевизия","Предаване", "Партия", "Кандидат", "Линк"])
+    for row in out:
+        w.writerow(row)
